19743fd4e434088e96e57a4ad9920d80de6539bf
LENNOVO-TOM\houyu <houyuteng0501@163.com>
2023-08-03 11:56:33 +0800

    更新大部分数据库函数

--- Server/Controller/MachineController.py
+++ Server/Controller/MachineController.py
@@ -5,17 +5,13 @@ def machineOnController(machineID, status):
     try:
         cnx = DBconnect.databaseConnect()
     except Exception as e:
-        print("Failed to connect to the database:", e)
-        return False
-
-    status = MachineDB.getMachineStatus(cnx, machineID)
-    print(f"现在机器{machineID}的状态是{status}")
-
-    MachineDB.machineOn_Off(cnx, machineID, status)
-    print(f"调整机器状态为{status}")
-
-    status = MachineDB.getMachineStatus(cnx, machineID)
-    print(f"现在机器{machineID}的状态是{status}")
-
-    DBconnect.databaseDisconnect(cnx)
-    return True
+        print("连接数据库失败：", e)
+        return f"连接数据库失败:{e}"
+    dbresult = MachineDB.machineOn_Off(cnx, machineID, status)
+    if dbresult == 1:
+        print(f"调整机器状态为{status}")
+        DBconnect.databaseDisconnect(cnx)
+        return 1
+    else:
+        print(f"机器开关数据库连接错误，{dbresult}")
+        return f"机器开关数据库连接错误，{dbresult}"
--- Server/Demo.py
+++ Server/Demo.py
@@ -1,6 +1,7 @@
 # 使用小写字母和下划线来命名变量和函数。例如，my_variable 和 my_function。
 # 使用大写字母开头的单词来命名类。例如，MyClass。
 # 使用大写字母和下划线来命名常量。例如，MY_CONSTANT。
+# 所有成功都返回1，失败返回字符串
 
 ##pip install flask
 from Controller import MachineController
@@ -16,7 +17,7 @@ def machineStart():
     data = request.get_json()  # 获取数据
     machine_id = data.get('machineID')  # 获取machineid
     result = MachineController.machineOnController(machine_id, 1)
-    if result == True:
+    if result == 1:
         return {"success": True, "code": 200}
     else:
         return {"success": False, "code": 500}
@@ -27,7 +28,7 @@ def machineStart():
     data = request.get_json()  # 获取数据
     machine_id = data.get('machineID')  # 获取machineid
     result = MachineController.machineOnController(machine_id, 1)
-    if result == True:
+    if result == 1:
         return {"success": True, "code": 200}
     else:
         return {"success": False, "code": 500}
