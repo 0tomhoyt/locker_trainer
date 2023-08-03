787306d234b55460db7203a8d601e41823413cdd
LENNOVO-TOM\houyu <houyuteng0501@163.com>
2023-08-03 19:39:51 +0800

    java socket client，待修改

--- Server/Test/SocketTest.py
+++ Server/Test/SocketTest.py
@@ -1,5 +1,6 @@
 import socket, json
 
+
 # import unittest
 # from Controller import MachineController, UserController
 # from unittest.mock import patch
@@ -19,78 +20,76 @@ def start_client(event, data):
 
         # 接收服务器的回应
         data = s.recv(1024)
-        print( json.loads(data.decode('utf-8')))
-
-
+        print(json.loads(data.decode('utf-8')))
 
 
 if __name__ == "__main__":
     print("测试1：机器开关，正确")
     event = 'machineStart'
     data = {'machineId': "1"}
-    start_client(event,data)
+    start_client(event, data)
     event = 'machineStop'
     data = {'machineId': "1"}
-    start_client(event,data)
+    start_client(event, data)
 
     print("测试2：机器开关，不存在的机器Id")
     event = 'machineStart'
     data = {'machineId': "555"}
-    start_client(event,data)
+    start_client(event, data)
 
     print("测试3：机器开关，不正确的data格式")
     event = 'machineStart'
     data = {'machineID': "555"}
-    start_client(event,data)
+    start_client(event, data)
 
     print("测试4：工人登陆，正确")
     event = 'workerLogin'
     data = {'username': "hyt",
-            "password":"hjqCYS1301",
-            "machineId":1,
-            "workstationId":1}
-    start_client(event,data)
+            "password": "hjqCYS1301",
+            "machineId": 1,
+            "workstationId": 1}
+    start_client(event, data)
 
     print("测试5：工人登陆，错误的password")
     event = 'workerLogin'
     data = {'username': "hyt",
-            "password":"hjqCYS1302",
-            "machineId":1,
-            "workstationId":1}
-    start_client(event,data)
+            "password": "hjqCYS1302",
+            "machineId": 1,
+            "workstationId": 1}
+    start_client(event, data)
 
     print("测试6：工人登陆，错误的machineId")
     event = 'workerLogin'
     data = {'username': "hyt",
-            "password":"hjqCYS1301",
-            "machineId":2,
-            "workstationId":1}
-    start_client(event,data)
+            "password": "hjqCYS1301",
+            "machineId": 2,
+            "workstationId": 1}
+    start_client(event, data)
 
     print("测试7：工人登陆，错误的workstationId")
     event = 'workerLogin'
     data = {'username': "hyt",
-            "password":"hjqCYS1301",
-            "machineId":1,
-            "workstationId":2}
-    start_client(event,data)
+            "password": "hjqCYS1301",
+            "machineId": 1,
+            "workstationId": 2}
+    start_client(event, data)
 
     print("测试8：工人登陆，错误的data格式")
     event = 'workerLogin'
     data = {'username': "hyt",
-            "password":"hjqCYS1301"}
-    start_client(event,data)
+            "password": "hjqCYS1301"}
+    start_client(event, data)
 
     print("测试9：工人登出，正确")
     event = 'workerLogout'
-    data = {"machineId":1,
-            "workstationId":1}
-    start_client(event,data)
+    data = {"machineId": 1,
+            "workstationId": 1}
+    start_client(event, data)
 
     print("测试10：工人登出，错误的机器")
     event = 'workerLogout'
-    data = {"machineId":2,
-            "workstationId":1}
-    start_client(event,data)
+    data = {"machineId": 2,
+            "workstationId": 1}
+    start_client(event, data)
 
     # unittest.main()
