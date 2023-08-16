0ff21b57518a4396b20dc26c90d75b534c17cac7
LENNOVO-TOM\houyu <houyuteng0501@163.com>
2023-08-16 13:30:03 +0800

    修改注册界面

--- UserInterface/controllers/RegisterController.java
+++ UserInterface/controllers/RegisterController.java
@@ -2,6 +2,7 @@ package controllers;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
 import org.json.JSONException;
@@ -27,80 +28,51 @@ public class RegisterController implements Controller, Initializable {
     private RadioButton adminButton;
     @FXML
     public boolean register_btn_click() throws IOException, JSONException {
-//        if(field_username == null)
-//            System.out.println("username is empty");
-//        if(field_password == null)
-//            System.out.println("password is empty");
-//        if(field_password_again == null)
-//            System.out.println("please enter password again");
-
-
         String username = field_username.getText();
         String password = field_password.getText();
         String passwordAgain = field_password_again.getText();
         String finalPassword = "1";
         int role = 2;
-        if (passwordAgain.equals(password))
-
+        if (passwordAgain.equals(password)){
             finalPassword = Tools.MD5hash(password);
+        }
+        else{
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setTitle("密码不一致");
+            alert.setHeaderText(null);
+            alert.setContentText("密码不一致");
+            alert.showAndWait();
+            return false;
+        }
+
         if(adminButton.isSelected())
             role = 1;
 
 
-        String finalPassword1 = finalPassword;
-        int finalRole = role;
-//        Future<String> future = Main.executorService.submit(() -> {
-//
-//            return Tools.socketConnect(registerJSON(finalPassword1, finalRole));
-//        });
-//
-//        Popup popup = MainController.showLoadingPopup("运行中");
-//
-//        try {
-//            JSONObject jsonObject = Tools.transferToJSONObject(future.get(5, TimeUnit.SECONDS));
-//            if (jsonObject.has("loginSuccess") && jsonObject.getInt("code") == 200 && jsonObject.getBoolean("loginSuccess")){
-//                int machineID = jsonObject.getInt("machineId");
-//                int workerStationID = worker.isAdmin() ? 0 : jsonObject.getInt("workstationId");
-//                if (machineID == worker.getMachineID() && workerStationID == worker.getWorkStationID()) {
-//                    updateWorker(jsonObject);
-//                    popup.hide();
-//                    afterLogin();
-//                    return true;
-//                }
-//                else {
-//                    popup.hide();
-//                    MainController.popUpAlter("ERROR","","");
-//                    return false;
-//                }
-//            }
-//            else {
-//                popup.hide();
-//                MainController.popUpAlter("ERROR","",Tools.unicodeToChinese(jsonObject.getString("message")));
-//                return false;
-//            }
-//        } catch (TimeoutException e) {
-//            // 超时处理
-//            popup.hide();
-//            MainController.popUpAlter("ERROR","Time UP","登录超时");
-//            return false;
-//        } catch (InterruptedException | ExecutionException e) {
-//            // 其他异常处理
-//            popup.hide();
-//            MainController.popUpAlter("ERROR","ERROR","登录失败");
-//            return false;
-//        } catch (JSONException e) {
-//            throw new RuntimeException(e);
-//        }
-
-
         String s = Tools.socketConnect(registerJSON(finalPassword,role));
         JSONObject jsonObject = Tools.transferToJSONObject(s);
-        System.out.println(jsonObject);
-        return false;
+        if (jsonObject.getInt("code") == 200){
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setTitle("注册成功");
+            alert.setHeaderText(null);
+            alert.setContentText("用户注册成功！");
+            alert.showAndWait();
+            return true;
+        }
+        else {
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setTitle("注册失败");
+            alert.setHeaderText(null);
+            alert.setContentText("注册失败");
+            alert.showAndWait();
+            return false;
+        }
+
     }
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        adminButton.setSelected(true);
         MainController.addController(this);
     }
 
