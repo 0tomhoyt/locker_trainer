bd6210085eea77b46ac58733cf555dfc32d770f1
LENNOVO-TOM\houyu <houyuteng0501@163.com>
2023-10-11 22:35:55 +0200

    更新，用户名不能重复

--- Server/Controller/UserController.py
+++ Server/Controller/UserController.py
@@ -157,6 +157,8 @@ def addUser( userName, password, role):
 
 
     new_token = Util.generate_auth_token(30)
-    UserDB.createUser(cnx, userName, password, role, new_token, '', datetime.today())
-
-    return json.dumps({ "message": "添加成功","authToken": new_token, "code": 200})
+    result = UserDB.createUser(cnx, userName, password, role, new_token, '', datetime.today())
+    if result == 1:
+        return json.dumps({ "message": "添加成功","authToken": new_token, "code": 200})
+    else:
+        return json.dumps({ f"message": "{result}","code": 500})
